4c3cb3813b18662d54c82f1aa885f54f2f5fca7b
Ivan Rogério Lucas Parmegiani <106553853+ivanrlp@users.noreply.github.com>
2024-09-08 22:37:54 -0300

    Add files via upload

---
+++ scripts/dados.js
@@ -0,0 +1,29 @@
+
+// é utilizada para declarar uma nova variável em JavaScript. Isso significa que estamos criando um espaço na memória do computador para armazenar um valor.
+
+let dados = [
+    {
+        imagem_filme: "images/divertida-mente.jpg",
+        poster_filme: "Pôster de Divertida Mente",
+        titulo: "Divertida Mente",
+        descricao: "Divertida Mente, uma animação emocionante da Pixar, nos leva a uma jornada pela mente de Riley, uma menina de 11 anos ",
+        link: "https://pt.wikipedia.org/wiki/Inside_Out",
+        tags: "divertidamente desenhodivertido dm1 todos"
+    },
+    {
+        imagem_filme: "images/divertida-mente-2.jpg",
+        poster_filme: "Pôster de Divertida Mente 2",
+        titulo: "Divertida Mente 2",
+        descricao: "Divertida Mente 2, Uma Nova Jornada Emocional, é a aguardada dofilme que conquistou o público e a crítica ao explorar o universo interior da mente humana. Nesta nova aventura, acompanhamos Riley, agora uma adolescente, e suas emoções em meio às mudanças e desafios da pré-adolescência.",
+        link: "https://pt.wikipedia.org/wiki/Inside_Out_2",
+        tags: "divertidamente2 desenho divertida2 dm2 todos"
+    },
+    {
+        imagem_filme: "images/meu-malvado-favorito_4.jpg",
+        poster_filme: "Pôster de Meu Malvado Favorito 4",
+        titulo: "Meu Malvado Favorito 4",
+        descricao: "Meu Malvado Favorito 4 é um filme de comédia de animação estadunidense produzido pela Illumination Entertainment e distribuído pela Universal Pictures.",
+        link: "https://pt.wikipedia.org/wiki/Despicable_Me_4",
+        tags: "malvadofavorito4 malfavorito4 mv4 todos"
+    }
+];
